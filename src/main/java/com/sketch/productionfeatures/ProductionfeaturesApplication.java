6e67f12761d58fc9f2d5b855a91230832dc4bf17
Tmax-3098 <ishaan.3098@gmail.com>
2025-12-31 13:45:34 +0530

    initial commit

---
+++ src/main/java/com/sketch/productionfeatures/ProductionfeaturesApplication.java
@@ -0,0 +1,13 @@
+package com.sketch.productionfeatures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionfeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductionfeaturesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sketch/productionfeatures/ProductionfeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sketch.productionfeatures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionfeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
